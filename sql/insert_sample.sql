fb0423769dc012e35ab3c02efebc4c8fca714b4c
Gambohunter <frederick.shagalov@gmail.com>
2023-08-09 13:11:46 +0200

    bugfix?

--- sql/insert_sample.sql
+++ sql/insert_sample.sql
@@ -1,6 +1,5 @@
 BEGIN TRANSACTION;
-DELETE from todo;
-DELETE from list;
+
 DELETE from sqlite_sequence;
 INSERT INTO user (username, password, role) VALUES ('stonksadmin', 'stonksadmin', 'admin');
 INSERT INTO user (username, password, role) VALUES ('stonksuser', 'stonksuser', 'user');
