5a35876866c956b5db011b79519cbc309982514d
LeoPilgram <leonhardpilgram@gmail.com>
2023-08-09 13:04:19 +0200

    insert sample angepasst

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,8 +1,9 @@
 
 CREATE TABLE user (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL DEFAULT 'user' CHECK(role IN ('admin', 'user'))
 );
 
 
