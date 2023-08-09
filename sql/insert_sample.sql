f8add34525ac8fcb1982b1dfbd7a1b7c7c46b9a3
LeoPilgram <leonhardpilgram@gmail.com>
2023-08-09 13:35:34 +0200

    Müll aus SQL-Dateien entfernt

--- sql/insert_sample.sql
+++ sql/insert_sample.sql
@@ -1,5 +1,4 @@
 BEGIN TRANSACTION;
-
 DELETE from sqlite_sequence;
 INSERT INTO user (username, password, role) VALUES ('stonksadmin', 'stonksadmin', 'admin');
 INSERT INTO user (username, password, role) VALUES ('stonksuser', 'stonksuser', 'user');
